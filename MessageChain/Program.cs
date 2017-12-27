22226eb1c1739d2829ef48763206ad99715cce03
William Terry <Tezza48is@aol.com>
2017-12-27 23:13:38 +0000

    Add project files.

---
+++ MessageChain/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
