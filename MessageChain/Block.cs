28a25493c7caa8f7adbce8b6017a1a1b75238a96
William Terry <Tezza48is@aol.com>
2017-12-28 02:37:00 +0000

    Proof of Work

--- MessageChain/Block.cs
+++ MessageChain/Block.cs
@@ -10,26 +10,47 @@ namespace MessageChain
 {
     class Block
     {
+        int m_Index;
         string m_PreveousHash;
         Message[] m_Messages;
+        int m_Nonce;
         string m_ThisHash;
 
         public string Hash { get => m_ThisHash; }
+        public int Index { get => m_Index; }
 
-        public Block(string prev, Message[] messages)
+        public Block(int index, string prev, Message[] messages)
         {
+            m_Index = index;
             m_PreveousHash = prev;
             m_Messages = messages;
-            m_ThisHash = CreateHash();
+            m_Nonce = 0;
+            m_ThisHash = "";
         }
 
-        private string CreateHash()
+        public void MineBlock(int difficulty)
+        {
+            Console.WriteLine("Mining block " + m_Index);
+            while (!m_ThisHash.StartsWith(new string('0', difficulty)))
+            {
+                m_Nonce++;
+                m_ThisHash = CalculateHash();
+            }
+        }
+
+        private string CalculateHash()
         {
             SHA256 sha = new SHA256Managed();
 
             byte[] data = Encoding.ASCII.GetBytes(ToString());
 
             byte[] digest = sha.ComputeHash(data);
+
+            string hash = "";
+            //foreach (byte item in digest)
+            //{
+            //    hash += Convert.ToString(item, 2).PadLeft(8, '0');
+            //}
 
             return Encoding.ASCII.GetString(digest);
         }
@@ -40,23 +61,25 @@ namespace MessageChain
 
             if (format == "f")
             {
+                output += "Index: " + m_Index + "\n";
                 output += "Parent: " + m_PreveousHash + "\n";
                 output += "Messages:\n";
                 foreach (Message message in m_Messages)
                 {
                     output += message.ToString(format) + "\n";
                 }
+                output += "Nonce: " + m_Nonce + "\n";
                 output += "Hash: " + m_ThisHash + "\n";
             }
             else
             {
+                output += m_Index;
                 output += m_PreveousHash;
                 foreach (Message message in m_Messages)
                 {
                     output += message.ToString();
                 }
-
-
+                output += m_Nonce;
             }
             return output;
         }
--- MessageChain/BlockChain.cs
+++ MessageChain/BlockChain.cs
@@ -9,20 +9,23 @@ namespace MessageChain
     class BlockChain
     {
         List<Block> m_Blocks;
+        int m_Difficulty = 3;
 
         public BlockChain()
         {
             m_Blocks = new List<Block>() { CreateGenesisBlock() };
+            m_Blocks.Last().MineBlock(m_Difficulty);
         }
 
         private Block CreateGenesisBlock()
         {
-            return new Block("0", new Message[] { new Message("All", "All", "Genesis Block") });
+            return new Block(0, "0", new Message[] { new Message("All", "All", "Genesis Block") });
         }
 
         public void AddBlock(Message[] messages)
         {
-            m_Blocks.Add(new Block(m_Blocks.Last().Hash, messages));
+            m_Blocks.Add(new Block(m_Blocks.Last().Index +1, m_Blocks.Last().Hash, messages));
+            m_Blocks.Last().MineBlock(m_Difficulty);
         }
 
         public override string ToString()
